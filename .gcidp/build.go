508a0a8908ffc3ea6f9cbd9480b5517a57e23c03
Diyor Khaydarov <diyor28july@gmail.com>
2024-03-07 01:06:53 +0500

    Build backend

--- .gcidp/build.go
+++ .gcidp/build.go
@@ -26,6 +26,23 @@ func Build(runner *pipeline.Runner) {
 	}
 	var branch, containerName, imageName string
 	branch = runner.Branch
+
+	containerName = fmt.Sprintf("%s-back-%s", projectName, branch)
+	imageName = fmt.Sprintf("%s-back:%s", projectName, branch)
+	runner.Pipeline(
+		docker.RmContainer(fmt.Sprintf("%s-postgres-%s", projectName, branch), true),
+		docker.Run(dbName, "postgres:15.1").Config(
+			docker.Volume(fmt.Sprintf("%s-%s-postgres-data", projectName, branch), "/var/lib/postgresql/data"),
+			docker.Env("POSTGRES_DB", "elxolding"),
+			docker.Env("POSTGRES_PASSWORD", "postgres"),
+			docker.Hostname("elxolding_db"),
+		),
+        docker.Build(imageName, "./back").Target("prod"),
+        docker.RmContainer(containerName, true),
+        docker.Run(containerName, imageName).Config(
+            docker.Expose(fmt.Sprintf("api-%s-%s.apollos.studio", branch, projectName), "80"),
+        ),
+	)
 	containerName = fmt.Sprintf("%s-app-%s", projectName, branch)
 	imageName = fmt.Sprintf("%s-app:%s", projectName, branch)
 	runner.Pipeline(
