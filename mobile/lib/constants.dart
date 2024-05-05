491760e9909ab5ce1b9f476c4015895b3104fa7c
Diyor Khaydarov <diyor28july@gmail.com>
2024-05-06 01:28:53 +0500

    A small update

--- mobile/lib/constants.dart
+++ mobile/lib/constants.dart
@@ -4,7 +4,8 @@ import 'package:flutter/cupertino.dart';
 
 var isLoggedIn = false;
 // const baseUrl = "https://api-staging-elxolding.apollos.studio";
-const baseUrl = "http://localhost:3030";
+// const baseUrl = "http://localhost:3030";
+const baseUrl = "http://rfid.local:3030";
 // const baseUrl = "https://5216-84-54-72-231.ngrok-free.app";
 
 FeathersClient feathersApp = FeathersClient(baseUrl);
